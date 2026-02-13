9f527e994416f8b898b5dbefebb91fd94a9094b2
Eduardo Kamo <eduardokamoz@gmail.com>
2026-02-13 07:35:41 -0300

    feat: add title and icon props to FokusButton

--- components/FokusButton/index.jsx
+++ components/FokusButton/index.jsx
@@ -1,9 +1,10 @@
 import { Pressable, StyleSheet, Text } from "react-native";
 
-export const FokusButton = ({ onPress }) => {
+export const FokusButton = ({ onPress, title, icon }) => {
   return (
     <Pressable style={styles.button} onPress={onPress}>
-      <Text style={styles.buttonText}>Começar</Text>
+      {icon}
+      <Text style={styles.buttonText}>{title}</Text>
     </Pressable>
   );
 };
@@ -13,6 +14,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#B872FF",
     borderRadius: 32,
     padding: 8,
+    flexDirection: "row",
+    gap: 12,
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonText: {
     textAlign: "center",
